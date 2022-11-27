671ce69d109eb5bb6566c403477952dd56c23dde
WSP99 <nosod3@naver.com>
2022-11-27 23:09:34 +0900

    init

---
+++ Firebase_DynamicLink/app/src/main/java/com/psw9999/firebase_dynamiclink/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.psw9999.firebase_dynamiclink
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
