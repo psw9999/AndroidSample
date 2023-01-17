498aaefb47f7d18e311e877da352df4a6d79d6ab
WSP99 <nosod3@naver.com>
2023-01-17 19:23:34 +0900

    [FEAT] Android 12 이전 스플래시 화면 구현

---
+++ Kakao_jwt_login/app/src/main/java/com/psw9999/kakao_jwt_login/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.psw9999.kakao_jwt_login
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+        finish()
+    }
+}
